b66642f3e8caff99b86496e61ac7a1849ca71996
spiche91 <spiche91@ITS1053577>
2022-11-21 14:30:37 -0600

    Removed unused methods from ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -139,15 +139,6 @@ void ofApp::mouseMoved(int x, int y ){
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
@@ -156,29 +147,4 @@ void ofApp::mouseReleased(int x, int y, int button){
 	{
 		playing = true;
 	}
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -19,14 +19,8 @@ class ofApp : public ofBaseApp{
 		void reset();
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+
 		Coordinate2D paddleCoord{100,600};
 		
 		Paddle paddle{ paddleCoord, 8 };
